7fc67849f78c570bda7a6412e31d23128362ba9b
nivethi-ai <nivethikagopalakrish@gmail.com>
2024-10-04 11:55:00 +0530

    merge the first question

---
+++ index.js
@@ -0,0 +1,13 @@
+var product="milk"
+console.log(product)
+var count=5
+var price=100
+total=price*count
+console.log(total)
+
+var price=100
+var product="milk"
+console.log(product)
+var Discount=10/100*100//discount=0.1
+total=price-Discount
+console.log(total)
